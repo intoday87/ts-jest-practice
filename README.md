ff236f175e7947082f4de32e7121488e5673dc97
Dong-Ho Lee <dong-ho.lee@Mac-Studio.local>
2024-01-24 20:45:22 +0900

    Fix yarn berry에서 prettier 동작 안되는 문제

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ $ yarn jest --watch
 # 특정 파일 패턴 glob
 $ yarn jest --testPathPattern src/someDir
 ```
+
+yarn berry를 사용해서 prettier가 동작하지 않는 문제 해결
+https://yarnpkg.com/getting-started/editor-sdks
+
+```zsh
+$ yarn dlx @yarnpkg/sdks vscode
+```
--- src/index.test.ts
+++ src/index.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from "@jest/globals"
 import { plus } from "./index"
 
 describe("test plus", () => {
-    test("test",  () => {
-        expect(plus(1, 2)).toEqual(3)
-    })
+	test("test", () => {
+		expect(plus(1, 2)).toEqual(3)
+	})
 })
