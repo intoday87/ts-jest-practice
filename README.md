d89c6cde22700f942900a5dcc77b10a268d24d40
Dong-Ho Lee <dong-ho.lee@Mac-Studio.local>
2024-01-24 20:01:13 +0900

    ts-jest 환경 구축
    - yarn berry
    - @jest/global

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ts-jest-practice
+
+ts-jest로 테스트를 해볼수 있는 환경 구축
+
+```zsh
+# 전체 실행
+$ yarn jest
+# watch
+$ yarn jest --watch
+# 특정 파일 패턴 glob
+$ yarn jest --testPathPattern src/someDir
+```
---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "@jest/globals"
+import { plus } from "./index"
+
+describe("test plus", () => {
+    test("test",  () => {
+        expect(plus(1, 2)).toEqual(3)
+    })
+})
